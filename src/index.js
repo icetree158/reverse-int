cea2b6c81319fc19239432c971ec3cb37cfb426c
icetree158 <79416180+icetree158@users.noreply.github.com>
2021-03-16 00:38:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 module.exports = function reverse (n) {
-  let String = "";
 
-    for (var i = n.length - 1; i >= 0; i--) {
-        String += n[i]; 
-    }
-
-    return String;
+    return n.split("").reverse().join("");
+    
 }
